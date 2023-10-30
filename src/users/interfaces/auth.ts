dd9c01e42e07571206f9e3ffd4d356fb30faa5dc
DawnWT <dwnssb@gmail.com>
2023-10-30 10:32:13 +0100

    feat: compat swagger

--- src/users/interfaces/auth.ts
+++ src/users/interfaces/auth.ts
@@ -1,4 +1,9 @@
-export interface LoginParams {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LoginParams {
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
 }
