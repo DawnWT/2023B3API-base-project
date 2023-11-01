d81533bddbeb63f658317e389d30cd30d2b5c83e
DawnWT <dwnssb@gmail.com>
2023-11-02 00:59:54 +0100

    fix: use service instead of repository

--- src/projects/services/project-users.service.ts
+++ src/projects/services/project-users.service.ts
@@ -4,14 +4,14 @@ import { Project } from '../entities/project.entity';
 import { ProjectUser } from '../entities/project-user.entity';
 import { Repository } from 'typeorm';
 import { UsersService } from '../../users/services/users.service';
+import { ProjectsService } from './projects.service';
 
 @Injectable()
 export class ProjectUsersService {
   constructor(
-    @InjectRepository(Project)
-    private readonly projectRepository: Repository<Project>,
     @InjectRepository(ProjectUser)
     private readonly projectUserRepository: Repository<ProjectUser>,
+    private readonly projectService: ProjectsService,
     private readonly userService: UsersService,
   ) {}
 }
