825c5763a4fce826d149655b88a1fd246d835aad
DawnWT <dwnssb@gmail.com>
2023-11-01 23:47:30 +0100

    fix: project module import

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,17 +1,16 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
-import { IsProjectManager } from '../users/guards/isProjectManager.guard.';
 import { UsersModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { Project } from './entities/project.entity';
-import { ProjectUser } from './entities/project-user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Project, ProjectUser]),
-    IsProjectManager,
+    TypeOrmModule.forFeature([User, Project]),
+    JwtModule.register({}),
     UsersModule,
   ],
   controllers: [ProjectsController],
