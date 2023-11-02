e267fb0dd16c5212a12eeb217828a3b099e5b9d7
DawnWT <dwnssb@gmail.com>
2023-11-02 16:42:32 +0100

    feat: inject projectUser service to controller

--- src/projects/controllers/project-users.controller.ts
+++ src/projects/controllers/project-users.controller.ts
@@ -1,11 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { ProjectsService } from '../services/projects.service';
 import { UsersService } from '../../users/services/users.service';
+import { ProjectUsersService } from '../services/project-users.service';
 
 @Controller('project-users')
 export class ProjectUsersController {
   constructor(
     private readonly projectsService: ProjectsService,
+    private readonly projectsUserService: ProjectUsersService,
     private readonly userService: UsersService,
   ) {}
 }
