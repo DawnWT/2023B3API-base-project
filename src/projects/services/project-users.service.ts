39e676b52593962a0fe113cc0ca50f61b74cb774
DawnWT <dwnssb@gmail.com>
2023-11-02 16:40:19 +0100

    feat: create 'create' to projectUser service

--- src/projects/services/project-users.service.ts
+++ src/projects/services/project-users.service.ts
@@ -14,4 +14,36 @@ export class ProjectUsersService {
     private readonly projectService: ProjectsService,
     private readonly userService: UsersService,
   ) {}
+
+  async create(
+    createProjectUserDto: CreateProjectUserDto,
+  ): Promise<Option<ProjectUser>> {
+    const { projectId, userId, startDate, endDate } = createProjectUserDto;
+
+    const projectOption = await this.projectService.findOne(projectId);
+    if (projectOption.isErr()) {
+      return Err('Project not found');
+    }
+
+    const userOption = await this.userService.findOne(userId);
+    if (userOption.isErr()) {
+      return Err('User not found');
+    }
+
+    const projectUser = new ProjectUser({
+      startDate,
+      endDate,
+      project: projectOption.content,
+      user: userOption.content,
+    });
+
+    try {
+      const savedProjectUser =
+        await this.projectUserRepository.save(projectUser);
+
+      return Ok(savedProjectUser);
+    } catch (error) {
+      return Err('Could not create project-user');
+    }
+  }
 }
