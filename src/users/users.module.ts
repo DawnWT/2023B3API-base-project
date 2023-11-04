f6b66b496395c855901582a15f67d28c42f5ab7c
DawnWT <dwnssb@gmail.com>
2023-11-04 23:35:03 +0100

    fix: remove guards from module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './services/auth.service';
-import { IsAuth } from './guards/is-auth.guard';
-import { IsAdmin } from './guards/is-admin.guard';
-import { IsProjectManager } from './guards/is-project-manager.guard';
 import { Project } from '../projects/entities/project.entity';
 import { ProjectUser } from '../projects/entities/project-user.entity';
 
@@ -20,9 +17,6 @@ import { ProjectUser } from '../projects/entities/project-user.entity';
   providers: [
     UsersService,
     AuthService,
-    IsAuth,
-    IsAdmin,
-    IsProjectManager,
     {
       provide: 'APP_PIPE',
       useValue: new ValidationPipe({
@@ -30,6 +24,6 @@ import { ProjectUser } from '../projects/entities/project-user.entity';
       }),
     },
   ],
-  exports: [IsAuth, IsAdmin, IsProjectManager, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
