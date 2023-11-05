eceefef980a856889a1d3d8f5dcb0282e10965b4
DawnWT <dwnssb@gmail.com>
2023-11-05 18:53:32 +0100

    feat: imports in event module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,12 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { EventsService } from './services/events.service';
 import { EventsController } from './controllers/events.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event])],
+  imports: [
+    TypeOrmModule.forFeature([Event]),
+    JwtModule.register({}),
+    UsersModule,
+  ],
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [
+    EventsService,
+    {
+      provide: 'APP_PIPE',
+      useValue: new ValidationPipe({
+        transform: true,
+      }),
+    },
+  ],
 })
 export class EventsModule {}
