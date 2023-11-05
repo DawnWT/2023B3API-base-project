5798af771e601e3135f992e28788e19fde79096c
DawnWT <dwnssb@gmail.com>
2023-11-05 15:48:32 +0100

    feat: create 'findAll' in service

--- src/events/services/events.service.ts
+++ src/events/services/events.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Event } from '../entities/event.entity';
+import {
+  BaseError,
+  DatabaseInternalError,
+  UnknownError,
+} from '../../types/error';
+import { Err, Ok, Option } from '../../types/option';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, TypeORMError } from 'typeorm';
 
 @Injectable()
 export class EventsService {
@@ -9,4 +15,20 @@ export class EventsService {
     @InjectRepository(Event)
     private readonly eventRepository: Repository<Event>,
   ) {}
+
+  async findAll(): Promise<Option<Array<Event>, BaseError>> {
+    try {
+      const events = await this.eventRepository.find();
+
+      return Ok(events);
+    } catch (error) {
+      if (error instanceof TypeORMError) {
+        return Err(new DatabaseInternalError(error));
+      }
+
+      if (error instanceof Error) {
+        return Err(new UnknownError(error));
+      }
+    }
+  }
 }
