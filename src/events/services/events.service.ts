08ce5b1e9498fe8381cf5cfc636669f2ccb4526b
DawnWT <dwnssb@gmail.com>
2023-11-05 18:55:20 +0100

    feat: create 'create' method in service

--- src/events/services/events.service.ts
+++ src/events/services/events.service.ts
@@ -9,6 +9,11 @@ import { Err, Ok, Option } from '../../types/option';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, TypeORMError } from 'typeorm';
 import { EventNotFoundException } from '../types/error';
+import { CreateEventDto } from '../dto/create-event.dto';
+import {
+  UserNotAvailableException,
+  UserNotFoundException,
+} from '../../users/types/error';
 import { UsersService } from '../../users/services/users.service';
 
 @Injectable()
@@ -18,6 +23,49 @@ export class EventsService {
     private readonly eventRepository: Repository<Event>,
     private readonly userService: UsersService,
   ) {}
+
+  async create(
+    createEventDto: CreateEventDto & { userId: string },
+  ): Promise<
+    Option<Event, UserNotFoundException | UserNotAvailableException | BaseError>
+  > {
+    const parsedDate = new Date(createEventDto.date);
+
+    const userIsAvailable = await this.userService.userIsAvailableForEvent(
+      createEventDto.userId,
+      parsedDate,
+    );
+
+    if (userIsAvailable.isErr()) {
+      return userIsAvailable;
+    }
+
+    if (!userIsAvailable.content) {
+      return Err(new UserNotAvailableException());
+    }
+
+    const user = await this.userService.findOne(createEventDto.userId);
+
+    if (user.isErr()) {
+      return user;
+    }
+
+    const event = new Event({ ...createEventDto, date: parsedDate });
+
+    try {
+      const savedEvent = await this.eventRepository.save(event);
+
+      return Ok(savedEvent);
+    } catch (error) {
+      if (error instanceof TypeORMError) {
+        return Err(new DatabaseInternalError(error));
+      }
+
+      if (error instanceof Error) {
+        return Err(new UnknownError(error));
+      }
+    }
+  }
 
   async findOne(
     id: string,
