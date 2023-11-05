29d8a880e248b97c9dc93ac8d73b21647fc5d90c
DawnWT <dwnssb@gmail.com>
2023-11-05 18:56:22 +0100

    feat: create '/events' post endpoint

--- src/events/controllers/events.controller.ts
+++ src/events/controllers/events.controller.ts
@@ -1,7 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { EventsService } from '../services/events.service';
+import { IsAuth } from '../../users/guards/is-auth.guard';
+import { CreateEventDto } from '../dto/create-event.dto';
+import { Request, Response } from 'express';
+import { Payload } from '../../types/payload';
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
+
+  @UseGuards(IsAuth)
+  @Post()
+  async create(
+    @Body() createEventDto: CreateEventDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const { id } = req['token'] as Payload;
+
+    const event = await this.eventsService.create({
+      ...createEventDto,
+      userId: id,
+    });
+
+    if (event.isErr()) {
+      if (event.error.type === 'UserNotFoundException') {
+        return res.status(HttpStatus.NOT_FOUND).send('User not found');
+      }
+
+      if (event.error.type === 'UserNotAvailableException') {
+        return res
+          .status(HttpStatus.UNAUTHORIZED)
+          .send('An Event already exist for this date');
+      }
+
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .send('Internal server error');
+    }
+
+    return res.status(HttpStatus.CREATED).send(event.content);
+  }
 }
